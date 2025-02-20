fab019d550aa7acc4d0d35f6e749ea10232fdade
DiegoOntiveros <dontiveros_23@alu.uabcs.mx>
2025-02-19 16:03:16 -0800

    commits
    comentarios

--- src/IDSTV/window.java
+++ src/IDSTV/window.java
@@ -17,8 +17,8 @@ this.setVisible(true);
 	 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	 this.setResizable(true);
-	 this.setMaximumSize(new Dimension (800,800));
-	 this.setMinimumSize(new Dimension(800,800));
+	 this.setMaximumSize(new Dimension (400,800));
+	 this.setMinimumSize(new Dimension(400,800));
 		 this.setLocationRelativeTo(null);
 
 	}
